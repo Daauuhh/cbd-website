89c723e09e37fd3fc2051a1a74bc864d42e1a4e0
TasakaSan <tasaka@foxmail.fr>
2020-05-18 17:13:09 +0200

    Login Page
    Create LoginPage && clean Index

--- index.php
+++ index.php
@@ -1,32 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>HCH CBD FRANCE</title>
-	<link rel="stylesheet" type="text/css" href="src/css/main.css">
-	<link rel="stylesheet" type="text/css" href="src/css/reset.css">
-	<link rel="icon" type="image/pngn" href="src/images/icons/favicon.png"><!-- il faut un .ico -->
-</head>
 <body>
 
-	<?php include('src/php/header.php'); ?>
+	<?php require('src/php/header.php'); ?>
 	
 	<section>
-		<div id="login-body">
-				<h1>S'identifier</h1>
-
-				<form method="post" action="index.php">
-					<input type="email" name="email" placeholder="Votre adresse email" required />
-					<input type="password" name="password" placeholder="Mot de passe" required />
-					<button type="submit">S'identifier</button>
-					<label id="option"><input type="checkbox" name="auto" checked />Se souvenir de moi</label>
-				</form>
-			
-
-				<p class="grey">Première visite sur HCH France ? <a href="inscription.php">Inscrivez-vous</a>.</p>
-		</div>
+		<p>bla bla bla</p>
 	</section>
 
-	<?php include('src/php/footer.php'); ?>
+	<?php require('src/php/footer.php'); ?>
 </body>
 </html>
---
+++ src/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HCH CBD FRANCE</title>
+	<link rel="stylesheet" type="text/css" href="src/css/main.css">
+	<link rel="stylesheet" type="text/css" href="src/css/reset.css">
+	<link rel="icon" type="image/pngn" href="src/images/icons/favicon.png"><!-- il faut un .ico -->
+</head>
+
+<header>
+	<div id="brand"><img src="Images/png/hch.png" alt="HCH"></div>
+</header>
