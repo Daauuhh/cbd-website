b9dc611e14a9a0fef2417361ef2bf94392eac49b
Morgan <morganpsyinfo@gmail.com>
2020-05-19 13:18:45 +0200

    Update index.php
    juste un test pour voir ^^

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 		
 		<section>
 			<p>bla bla bla</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsum eaque officia. Officiis eligendi corporis, eius rerum vitae natus illo distinctio, ullam maiores quas pariatur. Error ratione corporis doloribus nostrum?</p>
+			<p>blablabla</p>
 		</section>
 
 		<?php require('src/php/footer.php'); ?>
