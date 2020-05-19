49ad84937f35b227136a61da521d58a572e1e218
Morgan <morganpsyinfo@gmail.com>
2020-05-19 15:33:58 +0200

    index
    updd

--- index.php
+++ index.php
@@ -1,9 +1,68 @@
 	<body>
 		<?php require('src/php/header.php'); ?>
+
+		<div class="banniere" id="ban">hehe</div>
+		
+<!-- NAV BAR BOOTSTRAP -->
+		<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">HCH France</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Nos fleurs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Indica</a>
+          <a class="dropdown-item" href="#">Sativa</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      </li>
+      <li class="nav-item"><a class="nav-link" href="#">Résine</a>      </li>
+	  <li><a class="nav-link" href="#">E-Liquide</a></li>
+	  <li><a class="nav-link" href="#">Nos huiles</a></li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
 		<section>
 			<p>bla bla bla</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsum eaque officia. Officiis eligendi corporis, eius rerum vitae natus illo distinctio, ullam maiores quas pariatur. Error ratione corporis doloribus nostrum?</p>
+			
 			<p>blablabla</p>
 		</section>
 
