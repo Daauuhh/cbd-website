d3914fa3f89439d1819cdd02a2ff366db8f84286
Morgan <morganpsyinfo@gmail.com>
2020-05-19 18:44:21 +0200

    Up mise en page

--- index.php
+++ index.php
@@ -1,42 +1,37 @@
+<?php require('src/php/header.php'); ?>
 	<body>
-		<?php require('src/php/header.php'); ?>
-
-		<div class="banniere" id="ban">hehe</div>
+	<div class="banniere"><a href="#"><img class=img-responsive class=logo src="src/images/icons/logo-newreso.png"   alt="logo"></a> </div>
 		
-<!-- NAV BAR BOOTSTRAP -->
-		<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">HCH France</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-	  <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Nos fleurs
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Indica</a>
-          <a class="dropdown-item" href="#">Sativa</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      </li>
-      <li class="nav-item"><a class="nav-link" href="#">Résine</a>      </li>
-	  <li><a class="nav-link" href="#">E-Liquide</a></li>
-	  <li><a class="nav-link" href="#">Nos huiles</a></li>
-      
-      
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+	<div class="container">
+<!--CAROUSEL-->
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="src/images/carousel/cbd1.jpg" class="d-block w-100" alt="cbd1">
+    </div>
+    <div class="carousel-item">
+      <img src="src/images/carousel/cbd2.jpg" class="d-block w-100" alt="cbd2">
+    </div>
+    <div class="carousel-item">
+      <img src="src/images/carousel/cbd3.jpg" class="d-block w-100" alt="cb3">
+    </div>
   </div>
-</nav>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>	
+ 
 
 
 
@@ -59,13 +54,13 @@
 
 
 
-		
-		<section>
-			<p>bla bla bla</p>
 			
-			<p>blablabla</p>
-		</section>
-
+			<section>
+				<p>bla bla bla</p>
+				
+				<p>blablabla</p>
+			</section>
+		</div>
 		<?php require('src/php/footer.php'); ?>
 	</body>
 </html>
