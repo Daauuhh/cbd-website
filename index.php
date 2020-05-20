ca7fade3a1de9ea410ccf5fec5a61504b53f4754
Morgan <morganpsyinfo@gmail.com>
2020-05-20 18:23:22 +0200

    maj mep

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require('src/php/header.php'); ?>
+<?php require('src/php/navbar.php'); ?>
 	<body>
 	<div class="banniere"><a href="#"><img class=img-responsive class=logo src="src/images/icons/logo-newreso.png"   alt="logo"></a> </div>
 		
@@ -11,7 +12,7 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
-    <div class="carousel-item active">
+    <div class="carousel-item active ">
       <img src="src/images/carousel/cbd1.jpg" class="d-block w-100" alt="cbd1">
     </div>
     <div class="carousel-item">
@@ -31,6 +32,37 @@
   </a>
 
 </div>	
+
+<section>
+<div class="title-box mx-auto">
+Et si on vous parlait un peu de nous
+</div>
+<p>Spécialiste du CBD, nous avons sélectionné les meilleurs produits à base de chanvre pour vous proposer une expérience récréative optimale et améliorer votre bien-être au quotidien en toute légalité.<br>
+Tous nos produits en vente ont en effet un taux de THC inférieur au taux maximal légal autorisé en France et en Europe. <br>
+Les produits aux cannabinoïdes que nous vous proposons ont de plus été choisi pour leur qualité, aussi bien pour les matières premières entrant dans leur composition que pour les process de fabrication.<br>
+L’origine du chanvre CBD que nous vendons est connue et tracée.<br>
+En achetant nos fleurs de CBD, nos huiles, nos résines, nos concentrés ou nos gélules, vous avez la garantie de consommer un produit de qualité, non coupé avec des matières premières à l’origine douteuse, et avec un taux de CBD contrôlé. <br>
+Nous privilégions également des emballages qui vous permettent à la fois une bonne conservation du produit pour préserver toutes les propriétés du CBD et un dosage précis pour bénéficier de tous ses bienfaits.<br>
+En maîtrisant le dosage de CBD consommé à votre sensibilité et à vos besoins, vous êtes sûrs que les cannabinoïdes vous apporteront l’état de relaxation et d’apaisement voulu</p>
+</section>
+
+<div class="liseret mx-auto"></div>
+
+<div class="title-box mx-auto">
+Le choix de l'equipe
+</div>
+<div class="card-contain">
+  <div class="card-boutique">
+    <div class="card-comment">
+      <div class="rate">etoiiiiiles</div>
+      <div class="nom">Maelle </div>
+      <div class="card-date">Le 06/05/2018</div>
+      <div class="comment">Tres bon produit , livraison gratuite , bon gout , bonne odeur , quoi de plus ?</div>
+    </div>
+  </div>
+ 
+</div>
+
  
 
 
@@ -55,11 +87,6 @@
 
 
 			
-			<section>
-				<p>bla bla bla</p>
-				
-				<p>blablabla</p>
-			</section>
 		</div>
 		<?php require('src/php/footer.php'); ?>
 	</body>
