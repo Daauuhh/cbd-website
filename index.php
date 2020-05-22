057c2b2ebe906130f14f09cf4e447e6f4474890f
Morgan <morganpsyinfo@gmail.com>
2020-05-22 10:58:36 +0200

    up

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 
 </div>	
 
-<section>
+<section class="bloc1">
 <div class="title-box mx-auto">
 Et si on vous parlait un peu de nous
 </div>
@@ -48,46 +48,67 @@ En maîtrisant le dosage de CBD consommé à votre sensibilité et à vos besoin
 
 <div class="liseret mx-auto"></div>
 
+<div class="bloc1">
 <div class="title-box mx-auto">
 Le choix de l'equipe
 </div>
 <div class="card-contain">
-  <div class="card-boutique">
-    <div class="card-comment">
-      <div class="rate">etoiiiiiles</div>
-      <div class="nom">Maelle </div>
-      <div class="card-date">Le 06/05/2018</div>
-      <div class="comment">Tres bon produit , livraison gratuite , bon gout , bonne odeur , quoi de plus ?</div>
-    </div>
-  </div>
+
+      <div class="card-boutique"><img src="src/images/product/img_product_ogkush.jpeg " class="mx-auto"width="100%"    alt="og kush">
+      <div class="rate"></div>           
+            <div class="card-comment">
+            
+            <div class="nom">Maelle, </div>
+            <div class="card-date">Le 06/05/2018</div>
+            <div class="comment">Tres bon produit , livraison gratuite , bon gout , bonne odeur , quoi de plus ?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae nam quia optio aspernatur fugiat ratione incidunt eaque? Quam placeat sint, eos commodi eaque provident quo delectus atque natus voluptate accusantium.</div>
+          </div>
+        </div>
+
+      <div class="card-boutique"><img src="src/images/product/img_product_shark.jpeg " class="mx-auto"width="100%"    alt="og kush">
+      <div class="rate"></div>
+        <div class="card-comment">
+          
+          <div class="nom">Maelle, </div>
+          <div class="card-date">Le 06/05/2018</div>
+          <div class="comment">Tres bon produit , livraison gratuite , bon gout , bonne odeur , quoi de plus ? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis reprehenderit sit veniam quasi maxime illum accusamus error quos natus aspernatur. Harum reiciendis dolor adipisci deleniti libero aliquam repellendus hic maxime?</div>
+        </div>
+      </div>
+
+      <div class="card-boutique"><img src="src/images/product/img_product_passion.jpg " class="mx-auto"width="100%"    alt="og kush">
+      <div class="rate"></div>
+        <div class="card-comment">
+          
+          <div class="nom">Maelle, </div>
+          <div class="card-date">Le 06/05/2018</div>
+          <div class="comment">Tres bon produit , livraison gratuite , bon gout , bonne odeur , quoi de plus ? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus saepe amet inventore incidunt, eum consequatur optio sint placeat reprehenderit culpa perspiciatis, obcaecati sequi adipisci doloribus ratione. Quis similique harum voluptas.</div>
+        </div>
+      </div>
+      </div>
+      <div class="liseret mx-auto"></div>
+
+      <section class="bloc1">
+<div class="title-box mx-auto">
+Nos produits a base de CBD
+</div>
+<section class="bloc1">
+<div class="title-box mx-auto">
+Et si on vous parlait un peu de nous
+</div>
+<p>Spécialiste du CBD, nous avons sélectionné les meilleurs produits à base de chanvre pour vous proposer une expérience récréative optimale et améliorer votre bien-être au quotidien en toute légalité.<br>
+Tous nos produits en vente ont en effet un taux de THC inférieur au taux maximal légal autorisé en France et en Europe. <br>
+Les produits aux cannabinoïdes que nous vous proposons ont de plus été choisi pour leur qualité, aussi bien pour les matières premières entrant dans leur composition que pour les process de fabrication.<br>
+L’origine du chanvre CBD que nous vendons est connue et tracée.<br>
+En achetant nos fleurs de CBD, nos huiles, nos résines, nos concentrés ou nos gélules, vous avez la garantie de consommer un produit de qualité, non coupé avec des matières premières à l’origine douteuse, et avec un taux de CBD contrôlé. <br>
+Nous privilégions également des emballages qui vous permettent à la fois une bonne conservation du produit pour préserver toutes les propriétés du CBD et un dosage précis pour bénéficier de tous ses bienfaits.<br>
+En maîtrisant le dosage de CBD consommé à votre sensibilité et à vos besoins, vous êtes sûrs que les cannabinoïdes vous apporteront l’état de relaxation et d’apaisement voulu</p>
+</section>
+
+
+
+
+      </div>
  
 </div>
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-		</div>
 		<?php require('src/php/footer.php'); ?>
 	</body>
 </html>
